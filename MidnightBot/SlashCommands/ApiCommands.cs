f431556db9546d936d8532d1f9887fe04b0d77d5
Daniel Ohrin <xqiam@outlook.com>
2023-03-31 16:06:59 -0500

    Added /AP

--- MidnightBot/SlashCommands/ApiCommands.cs
+++ MidnightBot/SlashCommands/ApiCommands.cs
@@ -36,7 +36,47 @@ namespace MidnightBot.SlashCommands
                 {
                     embed.WithTitle(playerWithBal.Name).WithImageUrl($"https://minotar.net/helm/{playerName}/200.png");
                     embed.AddField("Balance", "$" + BotUtils.FormatNumber(playerWithBal.Balance));
-                    embed.AddField("Baltop Position", $"#{playerWithBal.Position}");
+                    embed.AddField("Leaderboard Position", $"#{playerWithBal.Position}");
+                }
+                else
+                {
+                    embed.WithTitle("**AN ERROR OCCURED**").WithDescription("An unknown error has occured. Please contact a developer.").WithColor(DiscordColor.DarkRed);
+                }
+            }
+            else
+            {
+                embed.WithTitle("**Error**").WithDescription($"Player **{playerName}** was not found.").WithColor(DiscordColor.DarkRed);
+            }
+
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(embed.Build()));
+        }
+        [SlashCommand("ap", "Returns the current AP of the specified player.")]
+        public async Task APCommand(InteractionContext ctx, [Option("player", "IGN of the player's balance you want to see.")] string playerName)
+        {
+            //! Create a "thinking..." response
+            await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
+
+            Player? player = null;
+            
+            try
+            {
+                player = await MidnightAPI.GetPlayer(playerName);
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + "\n" + ex.StackTrace);
+            }
+
+            DiscordEmbedBuilder embed = new DiscordEmbedBuilder().WithColor(DiscordColor.Purple);
+
+            if (player != null)
+            {
+                PlayerWithEconomy? playerWithBal = await MidnightAPI.GetBalance(player.Id, "AP");
+
+                if (playerWithBal != null)
+                {
+                    embed.WithTitle(playerWithBal.Name).WithImageUrl($"https://minotar.net/helm/{playerName}/200.png");
+                    embed.AddField("AP", BotUtils.FormatNumber(playerWithBal.Balance.Substring(0, playerWithBal.Balance.IndexOf("."))));
+                    embed.AddField("Leaderboard Position", $"#{playerWithBal.Position}");
                 }
                 else
                 {
