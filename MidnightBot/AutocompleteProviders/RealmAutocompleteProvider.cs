abd0d171a628f0a6447247de412f7f80e4bb11d2
Daniel Ohrin <110187145+DanielOhrin@users.noreply.github.com>
2023-04-01 13:50:58 -0500

    Created skeleton for alliance commands

---
+++ MidnightBot/AutocompleteProviders/RealmAutocompleteProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+
+namespace MidnightBot.AutocompleteProviders
+{
+    internal class RealmAutocompleteProvider : IAutocompleteProvider
+    {
+        public async Task<IEnumerable<DiscordAutoCompleteChoice>> Provider(AutocompleteContext ctx)
+        {
+            return new List<DiscordAutoCompleteChoice>
+            {
+                new DiscordAutoCompleteChoice("dwarf", "DWARF_POINTS"),
+                new DiscordAutoCompleteChoice("fairy", "FAIRY_POINTS"),
+                new DiscordAutoCompleteChoice("fishing", "FISHING_POINTS")
+            };
+        }
+    }
+}
---
+++ MidnightBot/SlashCommands/AllianceCommands.cs
@@ -0,0 +1,40 @@
+﻿using DSharpPlus.SlashCommands;
+
+using MidnightBot.AutocompleteProviders;
+
+namespace MidnightBot.SlashCommands
+{
+    [SlashCommandGroup("alliance", "Alliance commands")]
+    public class AllianceCommands : ApplicationCommandModule
+    {
+        [SlashCommand("info", "Returns basic information about an alliance.")]
+        public async Task AllianceInfoCommand(InteractionContext ctx, [Option("name", "Name of a player or alliance.")] string searchValue)
+        {
+
+        }
+
+        [SlashCommand("who", "Returns basic information about an alliance.")]
+        public async Task AllianceWhoCommand(InteractionContext ctx, [Option("name", "Name of a player or alliance.")] string searchValue)
+        {
+
+        }
+
+        [SlashCommand("bal", "Returns a list of the alliance's members, their rankings, and the alliance's total balance.")]
+        public async Task AllianceBalCommand(InteractionContext ctx, [Option("name", "Name of a player or alliance.")] string searchValue)
+        {
+
+        }
+
+        [SlashCommand("ap", "Returns a list of the alliance's members, their rankings, and the alliance's total ap.")]
+        public async Task AllianceAPCommand(InteractionContext ctx, [Option("name", "Name of a player or alliance.")] string searchValue)
+        {
+
+        }
+
+        [SlashCommand("realmpoints", "Returns a list of the alliance's members, their rankings, and the alliance's total points.")]
+        public async Task AllianceRealmPointsCommand(InteractionContext ctx, [Option("name", "Name of a player or alliance.")] string searchValue, [Option("realm", "Which realm's points to show.", true)][Autocomplete(typeof(RealmAutocompleteProvider))] string realmType)
+        {
+
+        }
+    }
+}
